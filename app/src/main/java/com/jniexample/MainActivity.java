6cb5d9ba928b502845e5724c8f2ac20c58f23345
gauravgoyal1211 <gauravgoyal1212@gmail.com>
2018-05-30 14:30:35 +0530

    Added Toast.

--- app/src/main/java/com/jniexample/MainActivity.java
+++ app/src/main/java/com/jniexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.jniexample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText("Result :" + sumTwoValues(45, 10));
+        Toast.makeText(this, "Testing", Toast.LENGTH_SHORT).show();
     }
 
     /**
